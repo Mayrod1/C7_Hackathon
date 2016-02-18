f07ef6f92956571c4eefeed9f4c8c1791e8002fe
Neal Cloud <neal.cloud@gmail.com>
2016-02-18 12:40:11 -0800

    #5 js skeleton - NC

---
+++ main.js
@@ -0,0 +1,44 @@
+/*
+* Object for storing global variables
+* possible: input value strings for jquery selections
+* possible: tweet location holder,  geolocation coordiantes when map click
+*
+* */
+
+
+/*
+* Document Ready function
+* holds button click events to activate correct functions
+* */
+
+
+/*
+*Function: getTweetsbySearch for tags function
+* params: takes in text input
+* does: makes api call and stores tweets
+* returns: an array of tweets collected by param search info
+* */
+
+
+/*
+* Function: getTweetsbyLocation,
+* params: takes in coordinates/city *type depends on twitter api search requirements
+* does: makes api call and stores tweets
+* returns: an array of tweets collected by param search info
+* */
+
+
+/*
+* Function: parseTweets,
+ * params: tweet objects?
+ * does: filters tweet info down to tweet text and location info
+ * returns: an array of tweet objects that holds tweet text and location data
+* */
+
+
+/*
+* Function: displayTweets
+* params: tweet array
+* does: loops through array and applies them to google map api
+* returns: updates html, returns nothing
+* */
