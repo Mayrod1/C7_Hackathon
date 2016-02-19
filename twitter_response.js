a6d3a5c1196ab2219fb6e8b3e46de13ae26f6bc5
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-02-19 11:06:37 -0800

    testing appending tweets

--- twitter_response.js
+++ twitter_response.js
@@ -3,8 +3,9 @@
  */
 
 /*
-* simpleTweet - instead of storing whole tweets, stores relevant information extracted from tweet
-* */
+ * simpleTweet - instead of storing whole tweets, stores relevant information extracted from tweet
+ * */
+
 function simpleTweet(name, text, lat, long) {
     this.screenName = name;
     this.tweetText = text;
@@ -13,8 +14,8 @@ function simpleTweet(name, text, lat, long) {
 }
 
 /*
-* newTweetObj - uses closure to make distinct tweet object, intended to be put into tweetResults array
-* */
+ * newTweetObj - uses closure to make distinct tweet object, intended to be put into tweetResults array
+ * */
 function newTweetObj(name, text, lat, long)
 {
     var tweetObj = new simpleTweet(name, text, lat, long);
@@ -22,85 +23,80 @@ function newTweetObj(name, text, lat, long)
 }
 
 
-var apiTwitter = new APItwitter();
+/*
+ * tweetResults - simplified data extracted from tweets
+ * */
 
-function APItwitter() {
-    /*
-    * PUBLIC VARIABLES
-    * tweetResults - simplified data extracted from tweets
-    * */
-    this.tweetResults = [];
+var tweetResults = [];
 
-    /*
-    * PUBLIC METHODS
-    * resetTweetResults - resets list of results
-    * findTweets - finds tweets with given text in a given area
-    * */
+/*
+ * resetTweetResults - no params, no return, empties out array tweetResults
+ * */
 
-    /*
-     * resetTweetResults - no params, no return, empties out array tweetResults
-     * */
-    function resetTweetResults()
-    {
-        this.tweetResults = [];
-    }
+function resetTweetResults()
+{
+    tweetResults = [];
+}
 
-    /*
-     * findTweets
-     * @params - searchfor (string, search term), latitude (float, search term),
-     *   longitude (float, search term), rad (float, search term)
-     *
-     *  finds tweets WITH geotags that mention searchFor, near the given location
-     *
-     * */
-    this.findTweets = function (searchFor, latitude, longitude, rad) {
-        console.log('shere');
+/*
+ * findTweets
+ * @params - searchfor (string, search term), latitude (float, search term),
+ *   longitude (float, search term), rad (float, search term)
+ *
+ *  finds tweets WITH geotags that mention searchFor, near the given location
+ *
+ * */
+function findTweets(searchFor, latitude, longitude, rad){
+    console.log('shere');
 
-        $.ajax({
-            dataType: "JSON",
-            api_key: "LEARNING",
-            method: "GET",
-            url: "http://s-apis.learningfuze.com/hackathon/twitter/index.php",
-            data: {
-                search_term: searchFor,
-                lat: latitude ,
-                long: longitude,
-                radius: rad
-            },
-            success: function(response){
-                console.log("response: ", response);
-                console.log("response.tweets.statuses: ", response.tweets.statuses);
-                for (var i = 0; i < response.tweets.statuses.length; i++) {
-                    if (response.tweets.statuses[i].geo != null){
-                        this.tweetResults.push(
-                            newTweetObj(
-                                response.tweets.statuses[i].user.screen_name,
-                                response.tweets.statuses[i].text,
-                                response.tweets.statuses[i].geo.coordinates[0],
-                                response.tweets.statuses[i].geo.coordinates[1]
-                            )
-                        );
-                    }
+    $.ajax({
+        dataType: "JSON",
+        api_key: "LEARNING",
+        method: "GET",
+        url: "http://s-apis.learningfuze.com/hackathon/twitter/index.php",
+        data: {search_term: searchFor,
+            lat: latitude ,
+            long: longitude,
+            radius: rad
+        },
+        success: function(response){
+            console.log("response: ", response);
+            console.log("response.tweets.statuses: ", response.tweets.statuses);
+            for (var i = 0; i < response.tweets.statuses.length; i++) {
+                if (response.tweets.statuses[i].geo != null){
+                    tweetResults.push(
+                        newTweetObj(
+                            response.tweets.statuses[i].user.screen_name,
+                            response.tweets.statuses[i].text,
+                            response.tweets.statuses[i].geo.coordinates[0],
+                            response.tweets.statuses[i].geo.coordinates[1]
+                        )
+                    );
                 }
-                return this.tweetResults;
-            },
-            error: function(response){
-                console.log("response: ", response);
             }
-        });
-    }
-
-    /*
-    * PRIVATE FUNCTIONS
-    *   none yet
-    * */
+            console.log("tweetResults: ", tweetResults);
+            return tweetResults;
+        },
+        error: function(response){
+            console.log("response: ", response);
+        }
+    });
 }
 
 
-
-
-
 $(document).ready(function () {
- apiTwitter.findTweets("cats", 33.6694, -117.8231, 50);
+    findTweets("", 33.6694, -117.8231, 50);
+
+    $('button').click(function() {
+        for (var i = 0; i < tweetResults.length; i++) {
+            $('body').append('<p>', {text: i});
+            /*$('.div1').append(tweetResults[i].screenName);
+             $('.div1').append(tweetResults[i].tweetText);
+             $('.div1').append(tweetResults[i].latitude);
+             $('.div1').append(tweetResults[i].longitude);*/
+
+            console.log(i);
+        }
+    });
 
 });
